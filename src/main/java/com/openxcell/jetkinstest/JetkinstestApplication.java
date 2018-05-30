3f1b45ef202e4aafc64d7280a2bea94bac2f5110
Bhavesh Patadiya <bhavesh@openxcelltechnolabs.com>
2018-05-30 11:39:11 +0530

    initial commit

---
+++ src/main/java/com/openxcell/jetkinstest/JetkinstestApplication.java
@@ -0,0 +1,12 @@
+package com.openxcell.jetkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetkinstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetkinstestApplication.class, args);
+	}
+}
