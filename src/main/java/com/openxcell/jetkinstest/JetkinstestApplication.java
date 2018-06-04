a7c8d9b78c05a9861d39baf0ce68f063d5e5fbed
Bhavesh Patadiya <bhavesh@openxcelltechnolabs.com>
2018-06-04 09:25:34 +0530

    changes done

--- src/main/java/com/openxcell/jetkinstest/JetkinstestApplication.java
+++ src/main/java/com/openxcell/jetkinstest/JetkinstestApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JetkinstestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JetkinstestApplication.class, args);
+		SpringApplication.run(JetkinstestApplication.class, args);z
 		// Comment adeddd,
+		// It changes
 	}
 }
