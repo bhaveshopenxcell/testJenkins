fb834a70bd801b0a4475f0e50666533acc83dd9e
Bhavesh Patadiya <bhavesh@openxcelltechnolabs.com>
2018-06-06 15:05:14 +0530

    -Minor changes in jenkinsfile

--- src/main/java/com/openxcell/jetkinstest/JetkinstestApplication.java
+++ src/main/java/com/openxcell/jetkinstest/JetkinstestApplication.java
@@ -8,7 +8,7 @@ public class JetkinstestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JetkinstestApplication.class, args);
-		// Comment adeddd,
+		// Comment added,
 		// It changes
 	}
 }
